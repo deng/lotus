f906cf139edb13587bd2920718597372ff0fdef2
rustman <moyuan25@qq.com>
2020-09-15 11:31:53 +0800

    add wdpost test

--- cmd/lotus-bench/main.go
+++ cmd/lotus-bench/main.go
@@ -86,6 +86,7 @@ func main() {
 			proveCmd,
 			sealBenchCmd,
 			importBenchCmd,
+			wdpostCmd,
 		},
 	}
 
---
+++ cmd/lotus-bench/wdpost.go
@@ -0,0 +1,233 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/docker/go-units"
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-bitfield"
+	"github.com/filecoin-project/go-jsonrpc"
+	paramfetch "github.com/filecoin-project/go-paramfetch"
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/chain/types"
+	lcli "github.com/filecoin-project/lotus/cli"
+	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper"
+	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper/basicfs"
+	saproof "github.com/filecoin-project/specs-actors/actors/runtime/proof"
+	"github.com/urfave/cli/v2"
+	"golang.org/x/xerrors"
+	"math/rand"
+	"os"
+	"time"
+)
+
+var wdpostCmd = &cli.Command{
+	Name:  "wdpost",
+	Usage: "Benchmark a wdpost computation",
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:  "storage-dir",
+			Value: "~/.lotus-bench",
+			Usage: "storage.json Path to the storage directory that will store sectors long term",
+		},
+		&cli.StringFlag{
+			Name:  "sector-size",
+			Value: "512MiB",
+			Usage: "size of the sectors in bytes, i.e. 32GiB",
+		},
+		&cli.BoolFlag{
+			Name:  "no-gpu",
+			Usage: "disable gpu usage for the benchmark run",
+		},
+		&cli.Uint64Flag{
+			Name:  "deadline-num",
+			Value: 0,
+		},
+		&cli.StringFlag{
+			Name:  "miner-addr",
+			Usage: "pass miner address (only necessary if using existing sectorbuilder)",
+			Value: "t01000",
+		},
+	},
+	Action: func(c *cli.Context) error {
+		if c.Bool("no-gpu") {
+			err := os.Setenv("BELLMAN_NO_GPU", "1")
+			if err != nil {
+				return xerrors.Errorf("setting no-gpu flag: %w", err)
+			}
+		}
+		// sector size
+		sectorSizeInt, err := units.RAMInBytes(c.String("sector-size"))
+		if err != nil {
+			return err
+		}
+		sectorSize := abi.SectorSize(sectorSizeInt)
+		spt, err := ffiwrapper.SealProofTypeFromSectorSize(sectorSize)
+		if err != nil {
+			return err
+		}
+		// 获取时空证明
+		if err := paramfetch.GetParams(lcli.ReqContext(c), build.ParametersJSON(), uint64(sectorSize)); err != nil {
+			return xerrors.Errorf("getting params: %w", err)
+		}
+		// miner address
+		maddr, err := address.NewFromString(c.String("miner-addr"))
+		if err != nil {
+			return err
+		}
+		amid, err := address.IDFromAddress(maddr)
+		if err != nil {
+			return err
+		}
+		mid := abi.ActorID(amid)
+		//创建时空证明提供者
+		cfg := &ffiwrapper.Config{
+			SealProofType: spt,
+		}
+		sbfs := &basicfs.Provider{
+			Root: c.String("storage-dir"),
+		}
+		sb, err := ffiwrapper.New(sbfs, cfg)
+		if err != nil {
+			return err
+		}
+
+		sinfos, err := getProves(c)
+		if len(sinfos) == 0 {
+			// nothing to prove..
+			return xerrors.Errorf(" nothing to prove..")
+		}
+
+		var challenge [32]byte
+		rand.Read(challenge[:])
+		start := time.Now()
+		log.Info("computing window PoSt start sectors ", len(sinfos))
+		wproof1, _, err := sb.GenerateWindowPoSt(context.TODO(), mid, sinfos, challenge[:])
+		if err != nil {
+			return err
+		}
+
+		wpvi1 := saproof.WindowPoStVerifyInfo{
+			Randomness:        challenge[:],
+			Proofs:            wproof1,
+			ChallengedSectors: sinfos,
+			Prover:            mid,
+		}
+		ok, err := ffiwrapper.ProofVerifier.VerifyWindowPoSt(context.TODO(), wpvi1)
+		if err != nil {
+			return err
+		}
+		if !ok {
+			log.Error("post verification failed")
+		}
+
+		elapsed := time.Since(start)
+
+		log.Infow("computing window PoSt", "elapsed", elapsed)
+		return nil
+	},
+}
+
+func sectorsForProof(c *cli.Context, actor address.Address, allSectors bitfield.BitField) ([]saproof.SectorInfo, error) {
+	//获取待认证的扇区
+	nodeApi, ncloser, err := lcli.GetFullNodeAPI(c)
+	if err != nil {
+		return nil, err
+	}
+	defer ncloser()
+
+	ctx := lcli.DaemonContext(c)
+	_, err = nodeApi.StateMinerPartitions(ctx, actor, c.Uint64("deadline-num"), types.EmptyTSK)
+	if err != nil {
+		return nil, xerrors.Errorf("getting partitions: %w", err)
+	}
+	sset, err := nodeApi.StateMinerSectors(ctx, actor, nil, true, types.EmptyTSK)
+	if err != nil {
+		return nil, err
+	}
+	if len(sset) == 0 {
+		log.Error("don't have any sectors")
+		return nil, nil
+	}
+	//for partIdx, partition := range partitions {
+	// TODO: Can do this in parallel
+	//toProve, err := partition.ActiveSectors()
+	//if err != nil {
+	//	return xerrors.Errorf("getting active sectors: %w", err)
+	//}
+	//toProve, err = bitfield.MergeBitFields(toProve, partition.Recoveries)
+	//if err != nil {
+	//	return  xerrors.Errorf("adding recoveries to set of sectors to prove: %w", err)
+	//}
+	//ssi, err := sectorsForProof(nodeApi,ctx,actor,partition.Faults)
+	//if err != nil {
+	//	return nil,xerrors.Errorf("getting sorted sector info: %w", err)
+	//}
+	//if len(ssi) == 0 {
+	//	continue
+	//}
+	//sinfos = append(sinfos, ssi...)
+	//for _, si := range ssi {
+	//	sidToPart[si.SectorNumber] = partIdx
+	//}
+	//}
+
+	sectorByID := make(map[uint64]saproof.SectorInfo)
+	for _, sector := range sset {
+		sectorByID[uint64(sector.ID)] = saproof.SectorInfo{
+			SectorNumber: sector.ID,
+			SealedCID:    sector.Info.SealedCID,
+			SealProof:    sector.Info.SealProof,
+		}
+	}
+
+	proofSectors := make([]saproof.SectorInfo, 0)
+	if err := allSectors.ForEach(func(sectorNo uint64) error {
+		if info, found := sectorByID[sectorNo]; found {
+			proofSectors = append(proofSectors, info)
+		}
+		return nil
+	}); err != nil {
+		return nil, xerrors.Errorf("iterating partition sector bitmap: %w", err)
+	}
+	return proofSectors, nil
+}
+
+func getProves(c *cli.Context) ([]saproof.SectorInfo, error) {
+	minerApi, closer, err := lcli.GetStorageMinerAPI(c,
+		jsonrpc.WithNoReconnect(),
+		jsonrpc.WithTimeout(30*time.Second))
+	if err != nil {
+		return nil, err
+	}
+	defer closer()
+	ctx := lcli.ReqContext(c)
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	sectors, err := minerApi.SectorsList(ctx)
+	if err != nil {
+		return nil, err
+	}
+	res := make([]saproof.SectorInfo, 0)
+
+	for _, sc := range sectors {
+		st, err := minerApi.SectorsStatus(ctx, sc, false)
+		if err != nil {
+			return nil, err
+		}
+		if st.State == "Proving" {
+			res = append(res, saproof.SectorInfo{
+				SectorNumber: st.SectorID,
+				SealedCID:    *st.CommR,
+				SealProof:    st.SealProof,
+			})
+		}
+	}
+	if len(res) == 0 {
+		return nil, fmt.Errorf("don't have proving section")
+	}
+
+	return res, nil
+}
